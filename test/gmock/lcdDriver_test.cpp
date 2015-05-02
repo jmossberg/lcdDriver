bbb3ba5a8b44a6bb2b88fd0bb0e53cb48e2ba936
Jacob Mossberg <jacob.c.mossberg@gmail.com>
2015-05-02 21:40:41 +0200

    Add new tests to verify that lcdDriver can be destroyed and that lcdDriver activates LCD command mode

--- test/gmock/lcdDriver_test.cpp
+++ test/gmock/lcdDriver_test.cpp
@@ -8,10 +8,28 @@ extern "C"
 
 TEST(lcdDriver, createLcdDriver)
 {
-   ASSERT_EQ(0, lcdDriver_create());
+  ASSERT_EQ(0, lcdDriver_create());
 }
 
-//TEST(lcdDriver, activateCommandMode)
-//{
-//    ASSERT_EQ(1,1);
-//}
+TEST(lcdDriver, destroyLcdDriver)
+{
+  ASSERT_EQ(0, lcdDriver_destroy());
+}
+
+TEST(lcdDriver, activateCommandMode)
+{
+  //Setup
+  uint8_t test_character = 'A';
+  lcdIO_create();
+  ASSERT_EQ(0, lcdDriver_create());
+
+  //Execute
+  ASSERT_EQ(0, lcdDriver_displayCharacter(&test_character));
+
+  //Verify
+  ASSERT_EQ(1, mock_lcdIO_getCountActivateCommandMode());
+
+  //Teardown
+  ASSERT_EQ(0, lcdDriver_destroy());
+  lcdIO_destroy();
+}
